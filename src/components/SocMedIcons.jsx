e51eccf6ec36de329f4504c5bab7599feeefc304
Michael Gatmaitan <mchlgtmtn@gmail.com>
2021-09-27 13:04:31 +0800

    Profile link added on Social media Icons

--- src/components/SocMedIcons.jsx
+++ src/components/SocMedIcons.jsx
@@ -3,12 +3,20 @@ import './styles/SocMedIcons.css';
 const SocMedIcons = () => {
 
   let icons = ['facebook', 'github', 'instagram', 'soundcloud', 'sololearn'];
-
+  let links = [
+    'https://www.facebook.com/michael.gatmaitan29',
+    'https://github.com/Michael-Gatmaitan',
+    'https://www.instagram.com/mchlgtmtn/',
+    'https://soundcloud.com/michael-gatmaitan',
+    'https://www.sololearn.com/profile/9807990'
+  ]
   return (
     <div className="socmed-icons">
-      {icons.map(icon => (
-        <div className="socmed-container">
-          <img src={`./svg/soc-meds-icons/white-icons/${icon}.svg`} alt={icon} />
+      {icons.map((icon, key) => (
+        <div className="socmed-container" key={key}>
+          <a href={links[key]}>
+            <img src={`./svg/soc-meds-icons/white-icons/${icon}.svg`} alt={icon} />
+          </a>
         </div>
       ))}
     </div>
