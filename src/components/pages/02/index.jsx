be6a5f79e4631493b881743f92cf10a5461e5285
Michael Gatmaitan <mchlgtmtn@gmail.com>
2021-10-02 02:02:57 +0800

    Page 02 progress. Tools created.

--- src/components/pages/02/index.jsx
+++ src/components/pages/02/index.jsx
@@ -12,6 +12,16 @@ const Page02 = () => {
     { language: "TS", percentage: 60, key: 5 },
   ]
 
+  const toolsUsed = [
+    {
+      subHeaderTitle: "Coding",
+      toolIcons: ['git', 'vscode', 'vscode-insiders', 'sublime-text']
+    }, {
+      subHeaderTitle: "Designing",
+      toolIcons: ['adobe-xd', 'figma']
+    }
+  ];
+
   return (
     <div className="page page-02">
 
@@ -48,9 +58,31 @@ const Page02 = () => {
 
       </div>
 
-      <div className="tools-contianer">
+      <div className="tools-container">
         <div className="tools-wrapper">
           
+          <div className="tools">
+            <div className="tools-header-container">
+              <div className="tools-header">Tools I use</div>
+              <div className="close-tools">
+                <img src="./svg/body-icons/close-tools.svg" alt="" />
+              </div>
+            </div>
+
+            {toolsUsed.map((tool, i) => (
+              <ToolsUsedSection
+                subHeaderTitle={tool.subHeaderTitle}
+                toolIcons={tool.toolIcons}
+                key={i}
+              />
+            ))}
+
+          </div>
+
+          <button className="see-resume">
+            SEE RESUME
+          </button>
+
         </div>
       </div>
       
@@ -59,6 +91,19 @@ const Page02 = () => {
     </div>
   )
 }
+
+const ToolsUsedSection = ({ subHeaderTitle, toolIcons }) => (
+  <div className="tools-used-section" style={{ marginTop: 20 }}>
+    <div className="tools-sub-header">{subHeaderTitle}</div>
+    <div className="tool-icons-container">
+      {toolIcons.map((toolIcon, i) => (
+        <div className="tool-icon" key={i}>
+          <img src={`./svg/tools-logo/${toolIcon}.svg`} alt="" />
+        </div>
+      ))}
+    </div>
+  </div>
+)
 
 const StatBar = ({ languageLabel, percentage }) => {
 
